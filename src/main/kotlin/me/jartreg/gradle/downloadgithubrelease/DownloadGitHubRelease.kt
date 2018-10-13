78cf69cdec9fdb40d1e85631d365405b4e45a3c5
Jartreg <jartreg@users.noreply.github.com>
2018-10-13 10:40:39 +0200

    Add renaming API

--- src/main/kotlin/me/jartreg/gradle/downloadgithubrelease/DownloadGitHubRelease.kt
+++ src/main/kotlin/me/jartreg/gradle/downloadgithubrelease/DownloadGitHubRelease.kt
@@ -5,6 +5,7 @@ import okhttp3.Request
 import okio.Okio.buffer
 import okio.Okio.sink
 import org.gradle.api.DefaultTask
+import org.gradle.api.Transformer
 import org.gradle.api.tasks.*
 import org.kohsuke.github.GitHub
 import java.io.File
@@ -23,6 +24,12 @@ open class DownloadGitHubRelease : DefaultTask() {
     val computedDestination: Any
         @OutputDirectory
         get() = destination ?: File(project.buildDir, "github/$repository/${tagName ?: "latest"}")
+
+    private var renamingAction: Transformer<String, String>? = null
+
+    fun rename(renamingAction: Transformer<String, String>) {
+        this.renamingAction = renamingAction
+    }
 
     @TaskAction
     fun download() {
